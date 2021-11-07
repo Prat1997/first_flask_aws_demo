3543042acb0d0176e23e9bc9fa0494adc38337d0
Prat1997 <singhprateek21sept@gmail.com>
2021-11-07 14:46:17 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,7 @@
+from flask import Flask
+import flask
+application=Flask(__name__)
+
+@application.route('/')
+def hello_world():
+    return "Hello , This is prateek's first elastic bean stalk project"
